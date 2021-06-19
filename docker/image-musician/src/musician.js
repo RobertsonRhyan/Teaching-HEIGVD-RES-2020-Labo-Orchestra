42aa7834dc6f3bf4b9e6ac9223481bde4e3b362a
Rhyan Robertson <rhyan.robertson@heig-vd.ch>
2021-06-19 11:50:35 +0200

    Clean-up

--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -28,61 +28,69 @@ var server = dgram.createSocket('udp4');
 /*
  * Instruments and the sound they make
  */
-const instrumentMap = {
-    piano : "ti-ta-ti",
-    trumpet : "pouet",
-    flute : "trulu",
-    violin : "gzi-gzi",
-    drum : "boum-boum"
-}
+const instrumentMap = new Map(
+    [
+        ["piano", "ti-ta-ti"],
+        ["trumpet", "pouet"],
+        ["flute", "trulu"],
+        ["violin", "gzi-gzi"],
+        ["drum", "boum-boum"]
+    ]
+)
 
 
-function Musician(instrumentRequest){
+/**
+ * Create a new musician
+ * Each musician has a UUID and plays an instrument
+ * Every second, a UDP datagram is sent with it's attributs
+ * @param {string} instrumentRequest 
+ */
+function Musician(instrumentRequest) {
 
+    if(instrumentMap.get(instrumentRequest) == undefined){
+        return;
+    }
     // Get musician uuid
     this.uuid = uuidv4();
-    
-    /* Debug */
-    // this.uuid = '47669dd6-970a-4933-b327-523a1e561d34';
+
+    /*
+     * DEBUG - Check that "activeSince" updates in Auditor.js if musician
+     * is inactive for more then 5sec and emitts again before TCP called. 
+     *
+    this.uuid = '47669dd6-970a-4933-b327-523a1e561d34';
+     * 
+     */
 
 
-    // Get current time
-    //var time = new Date()
+    // This function prepares a payload and sends it in a UDP dgram.
+    Musician.prototype.update = function () {
 
-    // This function prepares a payload and sends it.
-    Musician.prototype.update = function(){
-        
         /*
          * Create a dynamic javascript object, 
-         * add the 4 properties (uuid, instrument, sound and activeSince)
+         * add the uuid and sound
          * and serialize the object to a JSON string
          */
-        var data = {
-            uuid : this.uuid,
-            //instrument : instrumentRequest,
-            sound : instrumentMap[instrumentRequest],
-            //activeSince : time.toJSON()
-        }
 
-        payload = JSON.stringify(data);
+        // Create {JSON} payload with sound emitted by instrument
+        let payload = JSON.stringify({
+            uuid: this.uuid,
+            sound: instrumentMap.get(instrumentRequest)
+        });
 
-        
-
-       /*
-	    * Finally, let's encapsulate the payload in a UDP datagram, which we publish on
-	    * the multicast address. All subscribers to this address will receive the message.
-	    */
+        /*
+         * Finally, let's encapsulate the payload in a UDP datagram, which we publish on
+         * the multicast address. All subscribers to this address will receive the message.
+         */
         message = new Buffer.from(payload);
-        server.send(message, 0, message.length, 
-            protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS, 
-            function(err, bytes){
-                console.log("Sending payload : "  + payload + " to port " + protocol.PROTOCOL_PORT + " via port " + server.address().port);
-            });
+        server.send(message, 0, message.length, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS, function (err, bytes) {
+            console.log("Sending payload : " + message + " to port " + protocol.PROTOCOL_PORT + " via port " + server.address().port);
+        });
     }
 
     // Call update every 1 second
-    setInterval(this.update.bind(this),1000);
+    setInterval(this.update.bind(this), 1000);
 }
 
 
-var m = new Musician(process.argv[2]);
+// Create new musician with parsed instrument.
+new Musician(process.argv[2]);
